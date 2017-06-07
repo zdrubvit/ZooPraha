54665a11b1b158d1099a5680e9460db646487766
Vit Zdrubecky <vit.zdrubecky@gmail.com>
2017-06-07 18:09:17 +0200

    New filter methods added to list mapper

--- app/src/main/java/cz/zdrubecky/zoopraha/lexicon_menu/TaxonomyListDataMapper.java
+++ app/src/main/java/cz/zdrubecky/zoopraha/lexicon_menu/TaxonomyListDataMapper.java
@@ -7,7 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 
 import cz.zdrubecky.zoopraha.manager.ClassificationManager;
+import cz.zdrubecky.zoopraha.manager.FilterManager;
 import cz.zdrubecky.zoopraha.model.Classification;
+import cz.zdrubecky.zoopraha.model.Filter;
 
 public class TaxonomyListDataMapper {
     public static HashMap<String, List<String>> getData(Context context) {
@@ -26,5 +28,29 @@ public class TaxonomyListDataMapper {
         }
 
         return expandableListData;
+    }
+
+    public static HashMap<String, List<String>> getFilterData(Context context, HashMap<String, String> groups) {
+        HashMap<String, List<String>> expandableListData = new HashMap<String, List<String>>();
+        FilterManager filterManager = new FilterManager(context);
+        String[] filterNames = new String[] {"biotopes", "continents", "food"};
+
+        for (HashMap.Entry<String, String> entry : groups.entrySet()) {
+            List<String> filterValues = getFilterValues(filterManager, entry.getKey());
+            expandableListData.put(entry.getValue(), filterValues);
+        }
+
+        return expandableListData;
+    }
+
+    public static List<String> getFilterValues(FilterManager filterManager, String filterName) {
+        List<Filter> filters = filterManager.getFilters(filterName);
+        List<String> filterValues = new ArrayList<>();
+
+        for (Filter filter : filters) {
+            filterValues.add(filter.getValue());
+        }
+
+        return filterValues;
     }
 }
