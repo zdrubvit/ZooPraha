34a2284d882292dfb5b77f2983d1e913bb1d9ff8
Vit Zdrubecky <vit.zdrubecky@gmail.com>
2017-06-09 12:27:40 +0200

    Image scaling set

--- app/src/main/java/cz/zdrubecky/zoopraha/api/ImageLoader.java
+++ app/src/main/java/cz/zdrubecky/zoopraha/api/ImageLoader.java
@@ -32,30 +32,29 @@ public class ImageLoader {
 
         // Beware the secure protocol or any other http redirection, Picasso can't cope with that and has to use a different downloader
         mPicasso = new Picasso.Builder(context)
-            .listener(new Picasso.Listener() {
-                @Override
-                public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
-                    Log.i(TAG, "An exception has been thrown during the image loading: " + exception.toString());
-                }
-            })
-            .downloader(new OkHttpDownloader(context))
-            .build();
+                .listener(new Picasso.Listener() {
+                    @Override
+                    public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                        Log.i(TAG, "An exception has been thrown during the image loading: " + exception.toString());
+                    }
+                })
+                .downloader(new OkHttpDownloader(context))
+                .build();
     }
 
     public void loadImage(String url, ImageView view) {
         // todo "cannot reset" error in some images
         mPicasso.load(url)
-            .placeholder(R.mipmap.image_placeholder)
-            .error(R.mipmap.image_broken)
-            .fit()
-            .into(view, new com.squareup.picasso.Callback() {
-                @Override
-                public void onSuccess() {}
+                .placeholder(R.mipmap.image_placeholder)
+                .error(R.mipmap.image_broken)
+                .into(view, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess() {}
 
-                @Override
-                public void onError() {
-                    Toast.makeText(mContext, R.string.datafetcher_error_loading_image, Toast.LENGTH_SHORT).show();
-                }
-            });
+                    @Override
+                    public void onError() {
+                        Toast.makeText(mContext, R.string.datafetcher_error_loading_image, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
